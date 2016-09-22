4f24c0d6c5d0f059cc60321edc3594d27fa5b153
forest-shadow <mamasay@yandex.ru>
2016-09-22 18:48:41 +0300

    home page markup process #01

--- dev/js/custom/main.js
+++ dev/js/custom/main.js
@@ -1,3 +1,12 @@
 $( document ).ready( function(){
-
+    $( '.mainpage-slider' ).slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 7000
+    });
 });
