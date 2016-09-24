f5b1692deca6a7bca59139f626894ea5e0499107
forest-shadow <mamasay@yandex.ru>
2016-09-24 04:18:25 +0300

    mainpage markup without popup #01

--- dev/js/custom/main.js
+++ dev/js/custom/main.js
@@ -9,4 +9,16 @@ $( document ).ready( function(){
         autoplay: true,
         autoplaySpeed: 7000
     });
+
+    $( '.project-carousel' ).slick({
+        arrows: true,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        cssEase: 'linear'
+        //autoplay: true,
+        //autoplaySpeed: 7000
+    });
 });
