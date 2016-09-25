99e8a76cf1d07537d6dabed5371a0849d16f8a1f
forest-shadow <mamasay@yandex.ru>
2016-09-25 18:07:52 +0300

    project page markup #01

--- dev/js/custom/main.js
+++ dev/js/custom/main.js
@@ -10,6 +10,17 @@ $( document ).ready( function(){
         autoplaySpeed: 7000
     });
 
+    $( '.project-desc-block__project-slider' ).slick({
+        arrows: true,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 7000
+    });
+
     $( '.project-carousel' ).slick({
         arrows: true,
         dots: true,
