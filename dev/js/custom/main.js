d3533db6f526f57875cf6c4243b29df32b090ad9
forest-shadow <mamasay@yandex.ru>
2016-09-28 22:42:32 +0300

    wp forms fixes

--- dev/js/custom/main.js
+++ dev/js/custom/main.js
@@ -44,7 +44,6 @@ $( document ).ready( function(){
     // 04. Feedback call popup
     $( '.feedback-call-popup' ).magnificPopup({
         type: 'inline',
-        preloader: false,
         focus: '#name',
 
         // When elemened is focused, some mobile browsers in some cases zoom in
@@ -70,6 +69,22 @@ $( document ).ready( function(){
         midClick: true,
         removalDelay: 300,
         mainClass: 'my-mfp-zoom-in'
+    });
+
+
+    $( '.project-planes__planes-gallery' ).magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
     });
 
     // 04. Calculator radio options opacity changers
